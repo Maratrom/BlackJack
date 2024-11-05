f0bb519b146cdb01607c82fb58f4febf14a65c2f
Maratrom <93550389+Maratrom@users.noreply.github.com>
2024-11-05 16:27:11 +0100

    update init backend

--- Backend/blackjack/settings.py
+++ Backend/blackjack/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost",
+    "http://127.0.0.1:5173",
+]
 
 # Application definition
 
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls.apps.PollsConfig'
 ]
 
 MIDDLEWARE = [
